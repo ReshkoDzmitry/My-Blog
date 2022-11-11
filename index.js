a62a5bbdcef095d8de23076a701ddd548b33c341
DzmitryR <dzmitryreshko@gmail.com>
2022-11-12 01:24:55 +0300

    added authorization user

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express';
-//import jwt from 'jsonwebtoken';
 import mongoose from "mongoose";
 import {registerValidation} from './validations/auth.js';
 import {validationResult} from "express-validator";
 import UserModel from './models/User.js'; //импортируем модель User с названием UserModel
 import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
 
 
 mongoose.connect('mongodb+srv://admin:qqqqqq@cluster0.0v9j9gg.mongodb.net/blog?retryWrites=true&w=majority') //подключаем с помощью mongoose базу данных mongoDB с логином и паролем указанными при создании БД
@@ -14,6 +14,44 @@ mongoose.connect('mongodb+srv://admin:qqqqqq@cluster0.0v9j9gg.mongodb.net/blog?r
 const app = express(); //создание express приложения. Вся логика приложения хранится в переменной app
 app.use(express.json()) //указываем, что с помощью use из express вытаскиваем json. Нужно чтобы приложение понимало json-формат
 
+app.post('/auth/login',  async (req, res) => { //авторизация
+    try {
+        const user = await UserModel.findOne({email: req.body.email}); //указываем, что нужно найти пользователя если он есть в БД
+        if (!user) { //если такой почты нет, то
+            return res.status(404).json({ //останавливаем код, выводим строку и ошибку 404
+                message: 'Пользователь не найден'
+            });
+        }
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash); //если пользователь нашёлся в БД, то сравнить его пароль в БД с тем, что он прислал
+        if (!isValidPass) { //если пароли не сходятся, то
+            return res.status(400).json({ //выводим строку и ошибку. Явно уточнять причину нельзя!
+                message: 'Неверный логин или пароль',
+            });
+        }
+        const token = jwt.sign( //если авторизовался, то создаём токен
+            {
+                _id: user._id, //шифруем именно id, т.к. id самая важная информация
+            },
+            'secret123', //ключ, по которому будет шифроваться токен (любой)
+            {
+                expiresIn: '30d', //время жизни токена (30 дней)
+            }
+        );
+
+        const {passwordHash, ...userData} = user._doc; //чтобы не показывать hash. Его использовать не будем. Ниже выводим только userData
+
+        res.json({ //возвращаем информацию о пользователе и токен
+            ...userData,
+            token,
+        });
+    }
+    catch (err) {
+        console.log(err) //выводим ошибку в консоль
+        res.status(500).json({ //выводим сообщение и код ошибки 500
+            message: 'Не удалось авторизоваться',
+        });
+    }
+});
 
 // app.get('/', (req, res) => {
 //     res.send('Hi');
@@ -38,29 +76,76 @@ app.use(express.json()) //указываем, что с помощью use из 
 
 
 app.post('/auth/register', registerValidation, async (req, res) => { //отлавливаем post-запрос по адресу /auth/register (auth - писать не обязательно). Вытаскиваем запрос req  и ответ res. Проверяем тело запроса и если всё ок, выполняем код дальше. Объявляем что функция будет async
-    const errors = validationResult(req); //получаем в переменную errors все ошибки. Передаём req из которого их и нужно вытащить
-    if (!errors.isEmpty()) { //если ошибки есть
-        return res.status(400).json(errors.array()); //возвращаем 400 статус (неверный запрос) со всеми ошибками
+    // const errors = validationResult(req); //получаем в переменную errors все ошибки. Передаём req из которого их и нужно вытащить
+    // if (!errors.isEmpty()) { //если ошибки есть
+    //     return res.status(400).json(errors.array()); //возвращаем 400 статус (неверный запрос) со всеми ошибками
+    // }
+    //
+    // const password = req.body.password; //вытаскиваем из req.body пароль
+    // const salt = await bcrypt.genSalt(10); //генерируем соль. await будет ошибкой, пока в параметрах функции не указать, что она async(см.выше!)
+    // const passwordHash = await bcrypt.hash(password, salt) //в переменной passwordHash будет хранится зашифрованный пароль. Передаём пароль (password) и алгоритм пароля (salt)
+    //
+    // const doc = new UserModel({ //подготавливаем документ на создание пользователя с помощью mongoDB. Передаём все что есть в req.body
+    //     email: req.body.email,
+    //     fullName: req.body.fullName,
+    //     avatarUrl: req.body.avatarUrl,
+    //     passwordHash, //переменная с зашифрованным паролем (см. выше)
+    // })
+    //
+    // const user = await doc.save(); //создаём пользователя в mongoDB и сохраняем в БД. Результат передаётся в переменную user
+    //
+    // // res.json({ //если ошибок нет
+    // //     success: true, //возвращаем строку
+    // // });
+    //
+    // res.json(user);//возвращаем информацию о пользователе
+
+
+    try { //если ошибок нет, то отрабатывает код:
+        const errors = validationResult(req); //получаем в переменную errors все ошибки. Передаём req из которого их и нужно вытащить
+        if (!errors.isEmpty()) { //если ошибки есть
+            return res.status(400).json(errors.array()); //возвращаем 400 статус (неверный запрос) со всеми ошибками
+        }
+
+        const password = req.body.password; //вытаскиваем из req.body пароль
+        const salt = await bcrypt.genSalt(10); //генерируем соль. await будет ошибкой, пока в параметрах функции не указать, что она async(см.выше!)
+        const hash = await bcrypt.hash(password, salt) //в переменной passwordHash будет хранится зашифрованный пароль. Передаём пароль (password) и алгоритм пароля (salt)
+
+        const doc = new UserModel({ //подготавливаем документ на создание пользователя с помощью mongoDB. Передаём все что есть в req.body
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash, //переменная с зашифрованным паролем (см. выше)
+        });
+
+        const user = await doc.save(); //создаём пользователя в mongoDB и сохраняем в БД. Результат передаётся в переменную user
+
+        const token = jwt.sign( //создаём токен с зашифрованной информацией
+            {
+                _id: user._id, //шифруем именно id, т.к. id самая важная информация
+            },
+            'secret123', //ключ, по которому будет шифроваться токен (любой)
+            {
+                expiresIn: '30d', //время жизни токена (30 дней)
+            }
+        );
+
+        // res.json({ //если ошибок нет
+        //     success: true, //возвращаем строку
+        // });
+
+        const {passwordHash, ...userData} = user._doc; //чтобы не показывать hash. Его использовать не будем. Ниже выводим только userData
+
+        res.json({ //возвращаем информацию о пользователе и токен
+            ...userData,
+            token,
+        });
+    } catch (err) { //если ошибка, то
+        console.log(err) //выводим ошибку в консоль
+        res.status(500).json({ //выводим сообщение и код ошибки 500
+            message: 'Не удалось зарегистрироваться',
+        });
     }
-
-    const password = req.body.password; //вытаскиваем из req.body пароль
-    const salt = await bcrypt.genSalt(10); //генерируем соль. await будет ошибкой, пока в параметрах функции не указать, что она async(см.выше!)
-    const passwordHash = await bcrypt.hash(password, salt) //в переменной passwordHash будет хранится зашифрованный пароль. Передаём пароль (password) и алгоритм пароля (salt)
-
-    const doc = new UserModel({ //подготавливаем документ на создание пользователя с помощью mongoDB. Передаём все что есть в req.body
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash, //переменная с зашифрованным паролем (см. выше)
-    })
-
-    const user = await doc.save(); //создаём пользователя в mongoDB и сохраняем в БД. Результат передаётся в переменную user
-
-    // res.json({ //если ошибок нет
-    //     success: true, //возвращаем строку
-    // });
-
-    res.json(user);//возвращаем информацию о пользователе
 
 });
 
